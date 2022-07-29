48463ec1ca626761abdf622934f7a9a6dd5a4269
Casey M <caseym@gmail.com>
2022-07-29 13:17:06 -0500

    revert back to json_works

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ Base = declarative_base()
 
 class Work(Base):
     __table_args__ = {"schema": "mid"}
-    __tablename__ = "json_works_fulltext_view"
+    __tablename__ = "json_works"
 
     id = Column(Numeric, primary_key=True)
     updated = Column(DateTime)
