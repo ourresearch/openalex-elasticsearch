188ef2b4feee27cbc9ccd481448bfd6d153a701a
Casey M <caseym@gmail.com>
2023-01-28 09:39:55 +0100

    modify script to delete authors

--- merge_away.py
+++ merge_away.py
@@ -10,20 +10,18 @@ if __name__ == "__main__":
     MERGE_AUTHORS_INDEX = "merge-authors"
     key = "https://openalex.org/A"
     count = 0
+    AUTHORS_INDEX = "authors-v10"
 
     for chunk in pd.read_csv("s3://openalex-sandbox/merge-away-authors-2022-01-19.csv.gz", chunksize=chunk_size):
-        document_list = []
+        actions = []
         for index, row in chunk.iterrows():
             count = count + 1
             openalex_id = f"{key}{row[0]}"
-            merge_into_id = "https://openalex.org/A4317838346"
-            doc = {
-                "id": openalex_id,
-                "merge_into_id": merge_into_id,
+            action = {
+                "_id": openalex_id,
+                "_op_type": "delete"
             }
-            document_list.append(doc)
+            actions.append(action)
 
-        if count > 4700000:
-            print(f"Count is {count}")
-            helpers.bulk(es_client, document_list, index=MERGE_AUTHORS_INDEX)
-
+        print(f"Count is {count}")
+        helpers.bulk(client=es_client, actions=actions, index=AUTHORS_INDEX)
