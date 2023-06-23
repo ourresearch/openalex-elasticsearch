04e1f70f933ab852ed74979ea1f50ae4346320c1
Casey M <caseym@gmail.com>
2023-06-23 11:06:19 -0500

    improve script

--- bulk_remove_duplicates.py
+++ bulk_remove_duplicates.py
@@ -11,31 +11,39 @@ def remove_duplicates():
 
     # loop run
     for chunk in pd.read_csv("s3://openalex-sandbox/work_ids_es_duplicates_20230619.txt", chunksize=chunk_size):
+        ids = []
         for index, row in chunk.iterrows():
             count = count + 1
-            if row[0] < 2129341946:
-                continue
             openalex_id = f"https://openalex.org/W{row[0]}"
-            find_id_and_delete(openalex_id)
+            ids.append(openalex_id)
+        s = Search(index=WORKS_INDEX)
+        s = s.extra(size=3000)
+        s = s.source(["id", "updated"])
+        s = s.filter("terms", id=ids)
+        response = s.execute()
+        elastic_ids = [r.id for r in response]
+        for openalex_id in ids:
+            if elastic_ids.count(openalex_id) > 1:
+                find_id_and_delete(openalex_id)
         print(count)
 
 
 def find_id_and_delete(id):
     s = Search(index=WORKS_INDEX)
     s = s.filter("term", id=id)
+    s = s.source(["id", "@timestamp"])
     s = s.sort("-@timestamp")
     response = s.execute()
-    if s.count() == 2:
-        record = response.hits[1]
-        delete_from_elastic(record.id, record.meta.index)
-    elif s.count() > 2:
-        print(f"id {id} in elastic more than 2 times.")
+    if s.count() > 1:
+        for record in response.hits[1:]:
+            delete_from_elastic(record.id, record.meta.index)
 
 
 def delete_from_elastic(duplicate_id, index):
     try:
         s = Search(index=index)
         s = s.filter("term", id=duplicate_id)
+        s = s.source(["id", "@timestamp"])
         s.delete()
         print(f"deleted duplicate id {duplicate_id} from index {index}")
     except ConflictError:
