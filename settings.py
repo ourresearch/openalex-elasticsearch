677c5eaca26bf6d8b91d15dfaf99ad9f90d32e9a
h1-the-swan <jason.portenoy@gmail.com>
2024-01-31 08:26:37 -0500

    works index version update

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import os
 
 ES_URL = os.environ.get("ES_URL_PROD", "http://elastic:testpass@127.0.0.1:9200")
-WORKS_INDEX = "works-v18-*,-*invalid-data"
+WORKS_INDEX = "works-v22-*,-*invalid-data"
 GROUPBY_VALUES_INDEX = "groupby_values"
